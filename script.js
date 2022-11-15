2a8730d1ba1fcb2a93ff71762c975f95d4165dd0
Texran <texranhamidzada@gmail.com>
2022-11-15 09:58:48 +0400

    fdas

---
+++ script.js
@@ -0,0 +1,8 @@
+let text = document.querySelector('input');
+let addButtonScript = document.querySelector('.buttonAdd');
+let xpicButtonScript = document.querySelector('.xpicButton');
+let sortButtonScript = document.querySelector('.sortButton');
+addButtonScript.addEventListener('click',(text)=>{
+    
+})
+
