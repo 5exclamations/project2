d04d798896c1d64527eff2445dc9183ea95d1ea2
Texran <texranhamidzada@gmail.com>
2022-11-16 09:11:11 +0400

    fsd

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let text = document.querySelector('input');
+let text = document.querySelector('.inputText');
+let listed = document.querySelector('.listofText')
 let addButtonScript = document.querySelector('.buttonAdd');
 let xpicButtonScript = document.querySelector('.xpicButton');
 let sortButtonScript = document.querySelector('.sortButton');
-addButtonScript.addEventListener('click',(text)=>{
-    
-})
+addButtonScript.addEventListener('click',(event)=>{
+    var createdElement = document.createElement('p');
+    createdElement.innerText = text.Value;
+    listed.appendChild(createdElement);
+    event.preventDefault();
+})  
 
